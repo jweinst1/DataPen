8ac9c72b9945c4ec969c1a587f38e021213332ad
Joshua Weinstein <jweinstein@splunk.com>
2018-01-14 12:51:13 -0800

    hello

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -7,6 +7,7 @@
 int main(int argc, char const *argv[]) {
         std::string foo = "Hello!\n";
         DataUnit strunit(foo);
+
         std::cout << strunit.getSize() << std::endl;
         return 0;
 
