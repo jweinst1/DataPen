f4957aff3037fb07c07399cfec40797d0bb82249
Joshua Weinstein <jweinstein@splunk.com>
2018-01-16 13:43:49 -0800

    new copy test done

--- src/test/DataCopyTest.h
+++ src/test/DataCopyTest.h
@@ -10,8 +10,18 @@ void CopyTest_1(void)
         unsigned char buf[50];
         long i = 5000;
         DataCopy::copyToData(i, buf);
-        std::cout << "[test copy]: size = 8, result = " << std::endl;
+        std::cout << "[test copy]: size = 8, result = "
+                  << static_cast<unsigned>(buf[0]) << ", "
+                  << static_cast<unsigned>(buf[1]) << ", "
+                  << static_cast<unsigned>(buf[2]) << ", "
+                  << static_cast<unsigned>(buf[3]) << ", "
+                  << static_cast<unsigned>(buf[4]) << ", "
+                  << static_cast<unsigned>(buf[5]) << ", "
+                  << static_cast<unsigned>(buf[6]) << ", "
+                  << static_cast<unsigned>(buf[7]) << " "
+                  << std::endl;
         if(buf[0] != 0) std::cout << "CopyTest_1: Passed" << std::endl;
+        else std::cout << "CopyTest_1: Failed" << std::endl;
 }
 
 #endif // DATAPEN_COPY_TEST_H
--- src/test/Main.cpp
+++ src/test/Main.cpp
@@ -1,4 +1,5 @@
 #include "DataBlockTest.h"
+#include "DataCopyTest.h"
 
 
 int main(int argc, char const *argv[]) {
@@ -6,5 +7,6 @@ int main(int argc, char const *argv[]) {
         BlockTest_2();
         BlockTest_3();
         BlockTest_4();
+        CopyTest_1();
         return 0;
 }
